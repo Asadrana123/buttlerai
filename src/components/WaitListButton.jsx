929d48ad86a15b88da3cf20646513df58ed6728f
Asad Rana <asadlukman246@gmail.com>
2024-11-21 17:44:24 +0530

    button updated

--- src/components/WaitListButton.jsx
+++ src/components/WaitListButton.jsx
@@ -3,23 +3,32 @@ import "./WaitListButton.css";
 import { useState } from "react";
 import Form from "./Form";
 const WaitlistButton = () => {
-  const [openModal,setOpenModal]=useState(false);
-  const handleOpen=()=>{
-        setOpenModal(true);
+  const [openModal, setOpenModal] = useState(false);
+  const handleOpen = () => {
+    setOpenModal(true);
   }
-  const handleClose=()=>{
-          setOpenModal(false);
+  const handleClose = () => {
+    setOpenModal(false);
   }
   return (
     <>
-    <button  onClick={handleOpen} className="waitlist-button">
+      {/* <button  onClick={handleOpen} className="waitlist-button">
       Join the waitlist
-    </button>
-     <Form
-      openModal={openModal}
-      onOpen={handleOpen}
-      onClose={handleClose}
-     />
+    </button> */}
+      <button
+        onClick={handleOpen}
+        class="waitlist-button">
+        Join the waitlist
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+      </button>
+      <Form
+        openModal={openModal}
+        onOpen={handleOpen}
+        onClose={handleClose}
+      />
     </>
   );
 };
